ecc699c5b3fde855551677b6fe66573cab2191dc
5nilha <bwtdirect@gmail.com>
2017-07-25 02:56:06 -0400

    Finishing Reviews editor

---
+++ db/migrate/20170725051544_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[5.1]
+  def change
+    create_table :reviews do |t|
+      t.string :author 
+      t.string :comment 
+      t.references :book  #The references column is needed because the Review model has a belongs_to association with the Book model.
+
+      t.timestamps
+    end
+  end
+end
